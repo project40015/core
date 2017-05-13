fd003bf08a0594057e92272c36c43a718fcba6ac
BuildTools <unconfigured@null.spigotmc.org>
2017-05-13 19:39:14 -0400

    Finished Harvester, ItemUtils, and StaffChat:
    Believe the Core is ready to replace Faction Essentials.

--- src/com/decimatepvp/utils/ItemUtils.java
+++ src/com/decimatepvp/utils/ItemUtils.java
@@ -1,11 +1,13 @@
 package com.decimatepvp.utils;
 
-import java.util.Arrays;
+import java.util.List;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import com.google.common.collect.Lists;
 
 public class ItemUtils {
 	
@@ -19,8 +21,12 @@ public class ItemUtils {
 
 	public static void setLore(ItemStack item, String[] lore) {
 		ItemMeta meta = item.getItemMeta();
-		meta.setLore(Arrays.asList(lore));
-		item.setItemMeta(meta);
+		List<String> coloredLore = Lists.newArrayList();
+		for(String string : lore) {
+			coloredLore.add(DecimateUtils.color(string));
+		}
+	meta.setLore(coloredLore);
+	item.setItemMeta(meta);
 	}
 
 	public static void setDisplayName(ItemStack item, String displayName) {
@@ -30,7 +36,7 @@ public class ItemUtils {
 	}
 
 	public static boolean isItemCloned(ItemStack i1, ItemStack i2) {
-		return (i1 != null) && (i2 != null) &&
+		return (i1 != null) && (i2 != null) && (i1.getItemMeta().hasDisplayName()) && (i2.getItemMeta().hasDisplayName()) &&
 				(i1.getItemMeta().getDisplayName().equals(i2.getItemMeta().getDisplayName())) &&
 				(i1.getType() == i2.getType());
 	}
