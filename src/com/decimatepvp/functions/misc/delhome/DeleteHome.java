1380707dda26c9c9d88e871ff4816045d8afd9f6
xTheGamerCodes <xthegamercodes@gmail.com>
2017-06-19 18:06:46 -0500

    Fixed teleportation glitch

--- src/com/decimatepvp/functions/misc/delhome/DeleteHome.java
+++ src/com/decimatepvp/functions/misc/delhome/DeleteHome.java
@@ -2,7 +2,6 @@ package com.decimatepvp.functions.misc.delhome;
 
 import java.util.Map;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
@@ -22,12 +21,14 @@ import com.massivecraft.factions.struct.Relation;
 public class DeleteHome implements Listener {
 
 	private Map<String, String> lastHomeCommand = Maps.newHashMap();
+	private Map<String, String> teleporting = Maps.newHashMap();
+	private Map<String, Timer> timer = Maps.newHashMap();
 	
 	@EventHandler
 	public void onPlayerTeleport(PlayerTeleportEvent event) {
 		Player player = event.getPlayer();
 		
-		if(lastHomeCommand.containsKey(player.getName())) {
+		if(teleporting.containsKey(player.getName())) {
 			Faction territory = FactionUtils.getFactionByLoc(event.getTo());
 			Relation rel = FactionUtils.getFaction(player).
 					getRelationTo(territory);
@@ -35,23 +36,19 @@ public class DeleteHome implements Listener {
 			if((rel != Relation.MEMBER) &&
 					(territory != FactionUtils.getWilderness())) {
 //				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), 
-//						"delhome " + player.getName() + ":" + lastHomeCommand.get(player.getName()));
+//						"delhome " + player.getName() + ":" + teleporting.get(player.getName()));
 				
-				player.sendMessage(ChatColor.RED + "You cannot teleport there!");
+				player.sendMessage(ChatColor.RED + "You cannot teleport there");
 				event.setCancelled(true);
 			}
 			
-			lastHomeCommand.remove(player.getName());
+			teleporting.remove(player.getName());
 		}
 	}
 	
 	@EventHandler
 	public void onHomeCommand(PlayerCommandPreprocessEvent event) {
 		Player player = event.getPlayer();
-		
-		if(lastHomeCommand.containsKey(player.getName())) {
-			return;
-		}
 		
 		String command = event.getMessage();
 		command = command.substring(1, command.length());
@@ -76,6 +73,14 @@ public class DeleteHome implements Listener {
 				event.setCancelled(true);
 				return;
 			}
+
+			if(timer.containsKey(player.getName())) {
+				player.sendMessage(ChatColor.DARK_RED + "Pending teleportation request cancelled.");
+				lastHomeCommand.remove(player);
+				timer.get(player.getName()).cancel();
+				timer.remove(player.getName());
+				lastHomeCommand.remove(player.getName());
+			}
 			
 			event.setCancelled(true);
 			lastHomeCommand.put(player.getName(), args[1]);
@@ -86,38 +91,65 @@ public class DeleteHome implements Listener {
 	}
 
 	private void startDelay(Player player) {
-		OfflinePlayer plyr = player;
-		String name = player.getName();
-		Location start = player.getLocation();
-		BukkitRunnable br = new BukkitRunnable() {
-			
-			int time = 0;
-			
-			@Override
-			public void run() {
-				if(plyr.isOnline()) {
-					if(start.distance(player.getLocation()) > 0.5D) {
-						player.sendMessage(ChatColor.DARK_RED + "Pending teleportation request cancelled.");
-						lastHomeCommand.remove(name);
-						cancel();
-					}
-					
-					if(time == 5) {
-						Bukkit.broadcastMessage("1");
-						player.performCommand("home " + lastHomeCommand.get(player.getName()));
-						cancel();
-					}
-				}
-				else {
-					lastHomeCommand.remove(name);
+		Timer br = new Timer(player);
+		
+		timer.put(player.getName(), br);
+		
+		br.runTaskTimer(DecimateCore.getCore(), 0, 20);
+	}
+	
+	class Timer extends BukkitRunnable {
+		
+		public boolean isRunning = false;
+		
+		OfflinePlayer plyr;
+		Player player;
+		Location start;
+		
+		int time = 0;
+		
+		public Timer(Player player) {
+			this.plyr = player;
+			this.player = player;
+			this.start = player.getLocation();
+		}
+		
+		@Override
+		public synchronized void cancel() throws IllegalStateException {
+			isRunning = false;
+			super.cancel();
+		}
+
+		@Override
+		public void run() {
+			isRunning = true;
+			if(plyr.isOnline()) {
+				if(start.distance(player.getLocation()) > 0.5D) {
+					player.sendMessage(ChatColor.DARK_RED + "Pending teleportation request cancelled.");
+					lastHomeCommand.remove(plyr.getName());
+					timer.remove(player.getName());
 					cancel();
 				}
 				
-				time++;
+				if(time == 5) {
+					player.sendMessage(ChatColor.GOLD + "Teleportation commencing.");
+					teleporting.put(player.getName(), lastHomeCommand.get(player.getName()));
+					player.performCommand("home " + lastHomeCommand.get(player.getName()));
+					lastHomeCommand.remove(plyr.getName());
+					timer.remove(player.getName());
+					cancel();
+				}
 			}
-		};
+			else {
+				lastHomeCommand.remove(plyr.getName());
+				timer.remove(player.getName());
+				cancel();
+			}
+			
+			time++;
 		
-		br.runTaskTimer(DecimateCore.getCore(), 0, 20);
+		}
+		
 	}
 
 }
